d7ae8c93cbe8e5808f0d30994ffa962aae3f08b6
rip <corpusraiden@gmail.com>
2015-05-12 12:00:09 +1000

    Added PowerShell cmdlets
    Added 2x PowerShell cmdlets

---
+++ Disable-Cached-Mode-Outlook.ps1
@@ -0,0 +1,9 @@
+﻿#Outlook 2010
+New-Item -Path "HKCU:\SOFTWARE\Policies\Microsoft\Office\14.0\Outlook\Cached Mode" -Force
+New-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\Office\14.0\Outlook\Cached Mode" -Name "Enable" -Value 0 -PropertyType "DWORD" -Force
+
+#Outlook 2013
+New-Item -Path "HKCU:\SOFTWARE\Policies\Microsoft\Office\15.0\Outlook\Cached Mode" -Force
+New-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\Office\15.0\Outlook\Cached Mode" -Name "Enable" -Value 0 -PropertyType "DWORD" -Force
+
+#Switch the -Value property to 1, to re-enable.
---
+++ re-sync-git-profile.ps1
@@ -0,0 +1,4 @@
+﻿Set-ExecutionPolicy remotesigned -force
+cd ([environment]::GetFolderPath([environment+SpecialFolder]::MyDocuments))
+git clone git://github.com/ripcrispy/PowerShellProfile.git
+exit
