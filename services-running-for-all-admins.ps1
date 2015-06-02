44e23ab2dd03a9d525c03d244b715f9ad6bc80ce
rip <corpusraiden@gmail.com>
2015-06-03 09:37:05 +1000

    Added 2x cmdlets
    Created service check for username/admins

---
+++ services-running-for-all-admins.ps1
@@ -0,0 +1,8 @@
+﻿$service=Get-ADGroupMember "Domain Admins" | Get-ADUser
+$domain =  DOMAIN_NAME_GOES_HERE
+$Computers = Get-ADComputer -Filter "operatingsystem -like '*server*'"
+foreach ($s in $service) {
+    foreach ($i in $Computers) 
+    {
+        get-wmiobject Win32_Service -ComputerName $i.Name -ErrorAction SilentlyContinue | where-object {$_.StartName -eq "$domain\$($s.SamAccountName)" } | format-table $i.Name, Name, StartName, State
+    }
---
+++ services-running-for-username.ps1
@@ -0,0 +1,4 @@
+﻿$Service = read-host 'Enter Account Name (DOMAIN\Username):'
+$Computers = Get-ADComputer -Filter "operatingsystem -like '*server*'"
+foreach ($i in $Computers) 
+{get-wmiobject Win32_Service -ComputerName $i.Name -ErrorAction SilentlyContinue | where-object {$_.StartName -eq "$service" } | format-table $i.Name, Name, StartName, State}
