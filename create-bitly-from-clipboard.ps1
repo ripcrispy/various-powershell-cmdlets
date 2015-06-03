62f2d8f6d924fc9aba9d7276b137876ba25ffa91
rip <corpusraiden@gmail.com>
2015-06-03 13:40:51 +1000

    Created 2x scripts
    1x useful, 1x useless

---
+++ create-bitly-from-clipboard.ps1
@@ -0,0 +1,8 @@
+﻿# requires PS 5.0 for get-clipboard
+Function bit-ly
+{
+    $token = "ENTER_BITLY_TOKEN"
+    $longurl = [System.Windows.Forms.Clipboard]::GetText()
+    $bitly = "https://api-ssl.bitly.com/v3/shorten?access_token=$token&longurl=$longurl"
+    (Invoke-RestMethod -Uri $bitly).data.url | clip
+}
---
+++ fun-scripts/get-random-takeout-dest.ps1
@@ -0,0 +1,19 @@
+﻿# random take-out
+
+$response = 'Y'
+$takeOut = $null
+$takeOutList = @()
+$WriteOutList = $null
+$Choice = $null
+
+Do
+{
+    $takeOut = Read-Host 'Please enter a take-out destination'
+    $response = Read-Host 'Would you like to add an additional choice? (y/n) default: y'
+    $takeOutList += $takeOut
+}
+until ($response -eq 'n')
+
+$Choice = Get-Random $takeOutList
+
+Write-Host "$Choice" -ForegroundColor Yellow
