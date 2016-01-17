0ae30d8759b4f36e20e635f5d65919359764856f
rip <corpusraiden@gmail.com>
2016-01-18 10:56:05 +1100

    Script: Enable Component States on Hosted Exchange
    This is for hosted hybrid exchange.

---
+++ activate-exchange-component-states.ps1
@@ -0,0 +1,23 @@
+﻿. 'E:\Exchange\bin\RemoteExchange.ps1'
+Connect-ExchangeServer -auto
+
+$components = get-servercomponentstate -identity wbl1-msx-cas | where state -eq inactive | select Component
+$identity = "wbl1-msx-cas"
+$requester = "healthapi"
+$i = 1
+
+if ($components)
+{
+    #!empty
+    Write-Host "Inactive exchange server components FOUND!" -ForegroundColor Red
+    foreach ($component in $components) 
+    {
+        Write-Progress -Activity "Activating Components" -Status "Activating: $component" -PercentComplete ($i/$components.Count*100)
+        set-servercomponentstate -identity $identity -component $component.ToString() -requester $requester -state active
+    }
+}
+else
+{
+    #empty
+    Write-Host "No inactive exchange server components!" -ForegroundColor Green
+}
