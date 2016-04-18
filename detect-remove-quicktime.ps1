213357b43515c10d7daefb2d1e1003a5f20a2cc7
rip <corpusraiden@gmail.com>
2016-04-18 13:55:23 +1000

    Added Useful scripts/cmdlets

---
+++ detect-remove-quicktime.ps1
@@ -0,0 +1,14 @@
+﻿$qtVer = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall  |
+    Get-ItemProperty |
+        Where-Object {$_.DisplayName -match "quicktime" } |
+            Select-Object -Property DisplayName, UninstallString
+
+ForEach ($ver in $qtVer) {
+
+    If ($ver.UninstallString) {
+
+        $uninst = $ver.UninstallString
+        $uninst = $uninst -replace "/I", "/x "
+        Start-Process cmd -ArgumentList "/c $uninst /quiet /norestart" -NoNewWindow
+    }
+}
