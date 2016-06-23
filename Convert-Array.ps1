89b6bfe92b0b8f540f0d04b4f78ed47b8fef5f7b
rip <corpusraiden@gmail.com>
2016-06-23 14:29:30 +1000

    Pipe lined output in to an array.

---
+++ Convert-Array.ps1
@@ -0,0 +1,18 @@
+﻿function Convert-Array
+{
+#Usage:  $targetUsers = "String Line1
+#...
+#string Line N" | convert-array
+    $i = $($input)
+    If ($($i).gettype().name -eq "String") {
+        $output = $i.trim().replace("`r","`n").replace("`n`n","`n").split("`n") |foreach {$_.trim()}
+    } Else {
+        $output = @()
+        $i |foreach {
+            $output += $_
+        }
+    }
+    return $output 
+}
+Set-Alias -name Out-Array -value Convert-Array
+Set-alias -name oa -value convert-array
